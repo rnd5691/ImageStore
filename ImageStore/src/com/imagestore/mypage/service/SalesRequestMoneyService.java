153e59d677a029bffacc4e851012e6d260f1f395
admin <admin@admin-PC>
2017-11-10 16:36:49 +0900

    salesMoney db 연동하는 부분 올립니다.

---
+++ ImageStore/src/com/imagestore/mypage/service/SalesRequestMoneyService.java
@@ -0,0 +1,55 @@
+package com.imagestore.mypage.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.imagestore.action.Action;
+import com.imagestore.action.ActionFoward;
+import com.imagestore.member.MemberDTO;
+import com.imagestore.util.PageMaker;
+import com.imagestore.work.WorkDAO;
+import com.imagestore.work.WorkDTO;
+
+public class SalesRequestMoneyService implements Action {
+
+	@Override
+	public ActionFoward doProcess(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+		ActionFoward actionFoward = new ActionFoward();
+		HttpSession session = request.getSession();
+		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
+		WorkDAO workDAO = new WorkDAO();
+		
+		int curPage=1;
+		
+		try {
+			curPage = Integer.parseInt(request.getParameter("curPage"));
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			curPage=1;
+		}
+		
+		int totalCount = 0;
+		
+		try {
+			totalCount = workDAO.getTotalCount();
+			PageMaker pageMaker = new PageMaker(curPage, totalCount);
+			
+			List<WorkDTO> ar = workDAO.selectList(memberDTO.getUser_num(), pageMaker.getMakeRow());
+			request.setAttribute("list", ar);
+			request.setAttribute("makePage", pageMaker.getMakePage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		actionFoward.setCheck(true);
+		actionFoward.setPath("../WEB-INF/view/MYPAGE/salesMoney.jsp");
+				
+		return actionFoward;
+	}
+
+}
